af259e3ba7eeecda81dba0e2c18b55a0eaa5e623
ifonlymm <1123022920@qq.com>
2021-01-09 15:18:03 +0800

    issues_01 新增测试函数

--- test.go
+++ test.go
@@ -1,6 +1,7 @@
 
 
 func(this *User) Marshal_V1()([]byte,error) {
+	//ToDo 测试修改
 	returen proto.Marshal(this)
 }
 
@@ -8,4 +9,7 @@ func (this *User)Unmarshal_V1()(*User,error){
 	return nil,nil
 }
 
+func(this *User)Marshal_V2()([]byte,error){
+	return proto.Marshal(this)
+}
 
